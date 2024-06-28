f2dfbeda35b59b37ce79f7d3a95956ada55aac4e
André Mendes <152634414+andre-cmendes@users.noreply.github.com>
2024-06-28 17:13:55 +0100

    Update Items Fix

--- script.js
+++ script.js
@@ -2,35 +2,35 @@
 let sliderElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#button");
 
-let sizePassword = document.querySelector("#valor");
+let sizePassword = document.querySelector("#value");
 let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
 let charset = "abcdefghijklmnopqrstuvwxzABCDEFGHIJKLMNOPQRSTUVWXZ0123456789!@#$%&/()=/?<>-_.;,~^´`+*{}[]§£"
-let novaPassword = "";
+let newPassword = "";
 
 sizePassword.innerHTML = sliderElement.value;
 
-slider.oninput = function(){
+slider.oninput = function () {
 	sizePassword.innerHTML = this.value;
 }
 
-function generatePassword(){
+function generatePassword() {
 
 	let pass = "";
 
-	for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+	for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
 		pass += charset.charAt(Math.floor(Math.random() * n));
 	}
 
 	containerPassword.classList.remove("hide");
 	password.innerHTML = pass;
-	novaPassword = pass;
+	newPassword = pass;
 
 }
 
-function copyPassword(){
-	alert("Password foi copiada!")
-	navigator.clipboard.writeText(novaPassword);
+function copyPassword() {
+	alert("The password has copied!")
+	navigator.clipboard.writeText(newPassword);
 }
